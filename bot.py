154a34a33f01f49039a23726dda178983a01e6d1
Geetanjalii-01 <geetanjali.saini.ug22@nsut.ac.in>
2026-04-15 23:18:37 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,37 @@
+import telebot
+
+TOKEN = "YAHAN_NEW_TOKEN_DALNA"
+
+bot = telebot.TeleBot(TOKEN)
+
+users = {}
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    users[message.chat.id] = {"step":"name"}
+    bot.send_message(message.chat.id,"🎓 Welcome to PW COUPON CODE\n\nApna naam bhejo:")
+
+@bot.message_handler(func=lambda m: True)
+def all(message):
+    cid = message.chat.id
+
+    if cid not in users:
+        users[cid] = {"step":"name"}
+
+    step = users[cid]["step"]
+
+    if step == "name":
+        users[cid]["name"] = message.text
+        users[cid]["step"] = "phone"
+        bot.send_message(cid,"📱 Mobile Number bhejo:")
+
+    elif step == "phone":
+        users[cid]["phone"] = message.text
+        users[cid]["step"] = "class"
+        bot.send_message(cid,"📚 Class / Exam bhejo:")
+
+    elif step == "class":
+        bot.send_message(cid,"🎉 Offer Unlocked 🔥\nhttps://www.pw.live/")
+        users[cid]["step"] = "done"
+
+bot.infinity_polling()
